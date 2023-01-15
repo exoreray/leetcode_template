19ae7a285b61c0b325db54628e51cc4ea17a8f53
Rui Xi <xr@berkeley.edu>
2023-01-15 07:28:51 -0800

    updated two more template reverse and diff2

---
+++ Array&List/diff_array.py
@@ -0,0 +1,13 @@
+# 370. Range Addition
+from ast import List
+from itertools import accumulate
+
+
+class Solution:
+    def getModifiedArray(self, length: int, updates: List[List[int]]) -> List[int]:
+        diff = [0]*(length + 1)
+        for update in updates:
+            diff[update[0]] += update[2]
+            diff[update[1] + 1] -= update[2] # cautious, when -update, need to be one index afterward.
+        # accumulate function efficiently reconstruct diff_array back. Last index is redundant.
+        return list(accumulate(diff))[:-1] 
---
+++ Array&List/sliding_window.py
@@ -0,0 +1,18 @@
+from ast import List
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left = right = 0
+        result = 0
+        while right < len(nums):
+            '''
+            '''
+            right += 1
+            
+            while window_needs_shrink:
+                '''
+                '''
+                left += 1
+
+        return result
